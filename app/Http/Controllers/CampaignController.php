04b628d1efd7929bab6bd39935fb5c0c68c76207
qdjsx@outlook.com <736201089@qq.com>
2019-01-15 10:54:57 +0800

    '2'

---
+++ app/Http/Controllers/CampaignController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+
+
+class CampaignController extends Controller
+{
+
+    public  function jsx(){
+        $a = \DB::table('admin')->get();
+        var_dump($a);
+        var_dump(1);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',"CampaignController@jsx");
 
 Route::any('campaign/index',"CampaignController@jsx");
